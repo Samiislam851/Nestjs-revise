e1d97a9b508909142c6ccd07a8622e6bc2eb14de
samiislam851 <samisiam851@gmail.com>
2024-10-08 05:46:50 +0600

    users module added

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    // GET /users
+    // GET /users/:id
+    // POST /users/:id
+    // PATCH /users/:id
+    // DELETE /users/:id 
+}
